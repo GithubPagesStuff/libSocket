6d2f84cdee1d6847241083383c84eccbebda7328
comrade <aiden@aidens-macbook-pro.local>
2021-10-02 10:03:18 -0400

    Initial Commit

---
+++ libSocket/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  libSocket
+//
+//  Created by Aiden on 10/2/21.
+//  Copyright © 2021 bobles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
